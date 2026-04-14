02846d0a4aa4650687eff9d572fa4aa7f5e2a9fa
Vikas Kumar <vikassejwar009@gmail.com>
2026-04-14 10:32:14 +0530

    update

--- app/Http/Controllers/Backend/DashboardController.php
+++ app/Http/Controllers/Backend/DashboardController.php
@@ -2,13 +2,22 @@
 
 namespace App\Http\Controllers\Backend;
 
-use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Models\Category;
+use App\Models\Contact;
+use App\Models\Product;
+use App\Models\Service;
+use App\Models\SubCategory;
 
 class DashboardController
 {
     public function index()
     {
-        return view('backend.pages.dashboard');
+        return view('backend.pages.dashboard', [
+            'categories' => Category::count(),
+            'subcategories' => SubCategory::count(),
+            'services' => Service::count(),
+            'products' => Product::count(),
+            'leads' => Contact::count(),
+        ]);
     }
 }
